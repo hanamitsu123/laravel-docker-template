dfa2cdc9da6e3f8784459b76dde84841da41f691
hanamitsu123 <tokumitsu777@gmail.com>
2025-07-23 14:19:18 +0900

    feat:todo機能のmodelを追加しました。
    -appコンテナにmodelを作成しました。
    
    -TodoModelをtodosテーブルにマッピングしました。
    
    -TodoController.php:$todoにModelを継承したTodoオブジェクトを格納しました。
                       :$todosにCollection型オブジェクトを格納しました。

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Todo;
+
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todo.index');//viewオブジェクトには、htmlを生成する機構がある。render()?
+        $todo = new Todo();
+        $todos = $todo->all(); //all()は、Eloquentクラスメソッドです。テーブルの全行を取得します。
+        return view('todo.index'); //viewオブジェクトには、htmlを生成する機構がある。render()?
     }
 }
