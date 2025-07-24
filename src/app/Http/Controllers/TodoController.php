853cc84c54f4bdce0bb1e7a3ba5bbe290070476f
hanamitsu123 <tokumitsu777@gmail.com>
2025-07-24 11:07:33 +0900

    ◇ feat:DBにデータを保存する機能を実装しました。
    feat:web.php:ルート定義を追加しました。
    
    feat:TodoController.php:

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -14,10 +14,25 @@ class TodoController extends Controller
         $todos = $todo->all(); //all()は、Eloquentクラスメソッドです。テーブルの全行を取得します。
         return view('todo.index', ['todos' => $todos]); //viewオブジェクトには、htmlを生成する機構がある。render()?
     }
+
     public function create()
     {
         $todo = new Todo();
         $todos = $todo->all();
         return view('todo.create', ['todos' => $todos]);
+    }
+
+    public function store(Request $request)
+    {
+        $content = $request->input('content');
+        // 1. todosテーブルの1レコードを表すTodoクラスをインスタンス化
+        $todo = new Todo();
+        // 2. Todoインスタンスのカラム名のプロパティに保存したい値を代入
+        $todo->content = $content;
+        // 3. Todoインスタンスの`->save()`を実行してオブジェクトの状態をDBに保存するINSERT文を実行
+        $todo->save();
+
+        return redirect()->route('todo.index');
+        //redirect()はredirectorインスタンスが返されます。
     }
 }
